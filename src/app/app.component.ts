9f65a59887f7588ae03e5b42c3e97ace623d5549
Luis Rafael Rivera <30272643+RafaelBambu@users.noreply.github.com>
2017-10-15 21:10:56 -0500

    Rafa
    asdasd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public content = [];
+  public slideConfig = {
+    "slidesToShow": 3, "slidesToScroll": 1, "autoplay": true, "autoplaySpeed": 3000, "infinite": true,
+    arrows: false
+  };
+
+  constructor() {
+    this.content = [
+      {
+        image: 'assets/images/content/marco-antonio-cazares.jpg',
+        description: 'Los destinos más baratos de América Latina para viajar en 2017',
+        author: 'Por: Alejandra Sanchez',
+        type: 'Articulo'
+      },
+      {
+        image: 'assets/images/content/marco-antonio-cazares.jpg',
+        description: 'Los nuevos hallazgos de la ciencia desde el siglo XX hasta ahora',
+        author: 'Por: Juan Pablo Hernández',
+        type: 'Video'
+      },
+      {
+        image: 'assets/images/content/marco-antonio-cazares.jpg',
+        description: 'Los destinos más baratos de América Latina para viajar en 2017',
+        author: 'Por: Alejandra Sanchez',
+        type: 'Articulo'
+      },
+      {
+        image: 'assets/images/content/marco-antonio-cazares.jpg',
+        description: 'Los nuevos hallazgos de la ciencia desde el siglo XX hasta ahora',
+        author: 'Por: Juan Pablo Hernández',
+        type: 'Video'
+      },
+      {
+        image: 'assets/images/content/marco-antonio-cazares.jpg',
+        description: 'Los destinos más baratos de América Latina para viajar en 2017',
+        author: 'Por: Alejandra Sanchez',
+        type: 'Articulo'
+      },
+      {
+        image: 'assets/images/content/marco-antonio-cazares.jpg',
+        description: 'Los nuevos hallazgos de la ciencia desde el siglo XX hasta ahora',
+        author: 'Por: Juan Pablo Hernández',
+        type: 'Video'
+      },
+      {
+        image: 'assets/images/content/marco-antonio-cazares.jpg',
+        description: 'Los destinos más baratos de América Latina para viajar en 2017',
+        author: 'Por: Alejandra Sanchez',
+        type: 'Articulo'
+      }
+    ]
+  }
+  ngOnInit() { }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { environment } from '../environments/environment';
+import {MatButtonModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { SlickModule } from 'ngx-slick';
 
 @NgModule({
   imports: [
     BrowserModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    SlickModule.forRoot(),
   ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
